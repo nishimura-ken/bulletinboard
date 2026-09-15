179f297730d558df7619c0c95177bf3ad6ad7300
Ken Nishimura <nishiyan.0516@gmail.com>
2026-09-15 10:53:54 +0900

    React Routerの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,16 @@
-import { useEffect, useState } from 'react'
-import { Fragment } from 'react'
 import './App.css'
-
-type Thread = {
-  id: string;
-  title: string
-}
-
-const API_URL = "https://railway.bulletinboard.techtrain.dev/threads"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 
 function App() {
-  const [threads, setThreads] = useState<Thread[]>([])
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState("")
-
-  useEffect(() => {
-    fetch(API_URL)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("スレッド情報の取得に失敗しました")
-        }
-        return response.json()
-      })
-      .then((data) => {
-        setThreads(data)
-      })
-      .catch((error) => {
-        setError(error.message)
-      })
-      .finally(() => { 
-        setLoading(false)
-      })
-    }, [])
-    
-    if (loading) {
-      return <p>読み込み中...</p>
-    }
-    
-    if (error) { 
-      return <p>{error}</p>
-    } 
-    
-    return (
-      <Fragment>
-        <header>
-          <h3>掲示板</h3>
-        </header>
-        <section id="center">
-          <h4>新着スレッド</h4>
-          { threads.length === 0 ? (
-            <p>スレッドがありません。</p>
-          ) : ( 
-            <table>
-              <tbody> {threads.map((thread) => (
-                <tr key={thread.id}>
-                  <td>{thread.title}</td>
-                </tr>
-                ))}
-              </tbody>
-            </table>
-          )}
-        </section>
-      </Fragment>
-    )
-  }
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react'
+import { Fragment } from 'react'
+
+type Thread = {
+    id: string;
+    title: string
+}
+
+const API_URL = "https://railway.bulletinboard.techtrain.dev/threads"
+
+function Home() {
+    const [threads, setThreads] = useState<Thread[]>([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState("")
+
+    useEffect(() => {
+        fetch(API_URL)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("スレッド情報の取得に失敗しました")
+            }
+            return response.json()
+        })
+        .then((data) => {
+            setThreads(data)
+        })
+        .catch((error) => {
+            setError(error.message)
+        })
+        .finally(() => { 
+            setLoading(false)
+        })
+    }, [])
+    
+    if (loading) {
+      return <p>読み込み中...</p>
+    }
+    
+    if (error) { 
+      return <p>{error}</p>
+    } 
+    
+    return (
+        <Fragment>
+            <header>
+            <h3>掲示板</h3>
+            </header>
+            <section id="center">
+            <h4>新着スレッド</h4>
+                { threads.length === 0 ? (
+                    <p>スレッドがありません。</p>
+                ) : ( 
+                    <table>
+                    <tbody> {threads.map((thread) => (
+                        <tr key={thread.id}>
+                        <td>{thread.title}</td>
+                        </tr>
+                        ))}
+                    </tbody>
+                    </table>
+                )}
+            </section>
+        </Fragment>
+    )
+}
+
+export default Home
